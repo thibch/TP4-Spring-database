678b32e783770bdddda6938540d0b924618494d8
thibch <thibault.chone@protonmail.com>
2021-11-17 11:21:15 +0100

    Initial commit

---
+++ src/main/java/fr/ul/tp4spring/Tp4SpringApplication.java
@@ -0,0 +1,13 @@
+package fr.ul.tp4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ul/tp4spring/Tp4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.tp4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
